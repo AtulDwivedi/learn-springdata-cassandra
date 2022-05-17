eab5107f74c205ba79fbc67590132ca7d684d610
Atul Dwivedi <atul16dwivedi@gmail.com>
2022-05-17 14:47:40 +0530

    initial commit

---
+++ src/main/java/com/atuldwivedi/learn/springdatacassandra/LearnSpringDataCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.atuldwivedi.learn.springdatacassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringDataCassandraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringDataCassandraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atuldwivedi/learn/springdatacassandra/LearnSpringDataCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atuldwivedi.learn.springdatacassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringDataCassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
